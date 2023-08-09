d5ef0732c5eab32be2804aef9dc4d72e9ab6976e
Iain <Iain@DESKTOP-O9UC60V>
2023-08-09 15:36:08 -0400

    test

--- scripts/toggle_dark.js
+++ scripts/toggle_dark.js
@@ -1,14 +1,17 @@
+const g_lightOnIcon = ref("light_on.svg");
+const g_lightOffIcon = ref("light_off.svg");
+
 function toggleDark() {
 	let theme = localStorage.getItem("theme");
 	let icon = document.getElementById("toggle-dark-icon");
 
 	if (theme == "dark") {
 		theme = "light";
-		icon.src = "/light_on.svg";
+		icon.src = g_lightOnIcon;
 	}
 	else {
 		theme = "dark";
-		icon.src = "/light_off.svg";
+		icon.src = g_lightOffIcon;
 	}
 
 	window.document.documentElement.setAttribute("color-theme", theme);
@@ -19,7 +22,7 @@ function setInitialIcon() {
 	let theme = localStorage.getItem("theme");
 	let icon = document.getElementById("toggle-dark-icon");
 
-	icon.src = theme == "dark" ? "/light_off.svg" : "/light_on.svg";
+	icon.src = theme == "dark" ? g_lightOffIcon : g_lightOnIcon;
 }
 
 setInitialIcon();
