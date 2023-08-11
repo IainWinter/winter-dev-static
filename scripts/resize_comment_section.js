bef79404e96a69ac85c58bd2d34073c180107011
Iain <Iain@DESKTOP-O9UC60V>
2023-08-11 01:04:09 -0400

    published

--- scripts/resize_comment_section.js
+++ scripts/resize_comment_section.js
@@ -1,8 +1,5 @@
 let g_frame = document.getElementById("comment-section-frame");
-
-// function setFrameTheme() {
-// 	frame.current?.contentWindow?.window.postMessage({ theme: themeContext.theme }, "*");
-// }
+let g_commentTitle = document.getElementById("comments");
 
 function childMessageHandler(e) {
 	switch (e.data.reason) {
@@ -10,9 +7,16 @@ function childMessageHandler(e) {
 			g_frame.height = e.data.height;
 			break;
 		case "askTheme":
-			//setFrameTheme();
+			g_frame.contentWindow?.window.postMessage({ theme: localStorage.getItem("theme") }, "*");
+			break;
+		case "setScrollToComments":
+			g_commentTitle.scrollIntoView();
 			break;
 	}
 }
 
 window.addEventListener("message", childMessageHandler);
+
+subscribeToThemeChange((theme) => {
+	g_frame.contentWindow?.window.postMessage({ theme: theme }, "*");
+});
